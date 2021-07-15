b465772b36cf6c1b2104b8f33d8177c373c19f40
Nabha Kamath <kamathnabha@gmail.com>
2021-07-15 17:22:57 +0530

    - WIP: OTP screen

---
+++ iOS/customerserviceapp/customerserviceapp/ViewController/OtpViewController.swift
@@ -0,0 +1,9 @@
+//
+//  OtpViewController.swift
+//  customerserviceapp
+//
+//  Created by Nabha Kamath on 15/07/21.
+//  Copyright © 2021 Next e-Services. All rights reserved.
+//
+
+import Foundation
